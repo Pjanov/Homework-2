3ba042b2701e9dc856c8e0b9b399a5851297e7bc
Андрей <pjanov84@mail.ru>
2022-10-11 17:10:48 +0300

    Solved Task_2

--- Task_2.py
+++ Task_2.py
@@ -4,7 +4,22 @@
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 
+# Первый вариант:
+n = int(input('Введите N: '))
+
+res = 1
+print(f'N = {n} ->', end=' ')
+for i in range(1, n + 1):
+    res *= i
+    i += 1
+    print(res, end=' ')
 
 
+#Второй вариант:
+# n = int(input('Введите N: '))
 
+# spisok = [1]
+# for i in range(2, n + 1):
+#     spisok.append(spisok[i - 2] * i)
+# print(f'N = 4 -> {spisok}')    
 
