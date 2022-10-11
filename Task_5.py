ec6bf3fe9fffc2e3bb68894b59ec47242b96b3df
Андрей <pjanov84@mail.ru>
2022-10-11 17:12:57 +0300

    Solved Task_5

--- Task_5.py
+++ Task_5.py
@@ -1,9 +1,29 @@
-# Реализуйте алгоритм перемешивания элементов списка. 
+# Реализуйте алгоритм перемешивания элементов списка.
 # Без функции shuffle из модуля random, другие методы использовать можно.
 
 
+# using random.sample()
 
 
+import random
+
+# Первый вариант:
+random_spisok = []
+for i in range(10):
+    random_spisok.append(random.randint(-10, 10))
+print(f'Список заданный рандомно: {random_spisok}')
+
+res = random.sample(random_spisok, len(random_spisok))
+print(f'Перемешанный список: {res}')
 
 
+# Второй вариант:
+# spisok = []
+# for i in range(10):
+#     spisok.append(random.randint(-10, 10))
+# print(f'Список заданный рандомно: {spisok}')
 
+# for i in range(len(spisok)-1, 0, -1):
+#     j = random.randint(0, i + 1)
+#     spisok[i], spisok[j] = spisok[j], spisok[i]
+# print(f'Перемешанный список: {spisok}')
